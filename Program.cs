ac609fc5016d613bbd14309f88fe342367e9e31a
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-11-18 00:44:11 +0500

    Added the method that creates and fills the array

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿CreateFillArray();
+
+string[] CreateFillArray()
+{
+    System.Console.WriteLine("Enter how many elements there are in the array:");
+    int numberOfElements = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[numberOfElements];
+    string line;
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"Enter the {i+1} element:");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
