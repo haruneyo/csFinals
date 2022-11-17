1fbebee63015940936b5ceb41bf20395c8355d1a
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-11-18 01:22:44 +0500

    Finished the program and improved some code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
 ﻿string[] originalArray = CreateFillArray();
+string[] sortedArray = SortedArray(originalArray);
+System.Console.WriteLine("The original array is: ");
 PrintArray(originalArray);
+System.Console.WriteLine("The sorted array is: ");
+PrintArray(sortedArray);
 
 string[] CreateFillArray()
 {
     System.Console.WriteLine("Enter how many elements there are in the array:");
     int numberOfElements = Convert.ToInt32(Console.ReadLine());
     string[] array = new string[numberOfElements];
-    string line;
     for (int i = 0; i < array.Length; i++)
     {
         System.Console.WriteLine($"Enter the {i + 1} element:");
@@ -17,9 +20,15 @@ string[] CreateFillArray()
 
 void PrintArray(string[] array)
 {
-    System.Console.WriteLine("The array is: ");
     for (int i = 0; i < array.Length; i++)
     {
         System.Console.Write($"{array[i]} ");
     }
+    System.Console.WriteLine();
+}
+
+string[] SortedArray(string[] array)
+{
+    array = array.Where(value => value.Length < 4).ToArray();
+    return array;
 }
